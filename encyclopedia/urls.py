7b63ba2ab651a7afa3c9e66415443b56e097a305
Calum Bell <calumabell@googlemail.com>
2021-01-17 19:50:22 +0000

    wiki entries now render at url wiki/TITLE

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:name>", views.entry, name="entry"),
+    path("error", views.error, name="error")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    entry = util.get_entry(name)
+
+    if not entry:
+        return HttpResponseRedirect(reverse("error"))
+
+    return render(request, "encyclopedia/entry.html", {
+        "name" : name,
+        "entry" : entry
+    })
+
+def error(request):
+    return render(request, "encyclopedia/error.html")
