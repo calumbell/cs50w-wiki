ceb4374110fd2deab14ac96b80a0d459fdb59260
Calum Bell <calumabell@googlemail.com>
2021-01-18 20:58:38 +0000

    implemented entry search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.entry, name="entry"),
-    path("error", views.error, name="error")
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,13 +13,25 @@ def index(request):
 def entry(request, name):
     entry = util.get_entry(name)
 
-    if not entry:
-        return HttpResponseRedirect(reverse("error"))
-
     return render(request, "encyclopedia/entry.html", {
         "name" : name,
         "entry" : entry
     })
 
-def error(request):
-    return render(request, "encyclopedia/error.html")
+def search(request):
+    if request.method == "POST":
+        query = request.POST["q"]
+        entry = util.get_entry(query)
+        if entry:
+            url = reverse('entry', kwargs={'name': query})
+            return HttpResponseRedirect(url)
+        else:
+            entries = util.list_entries()
+            matches = []
+
+            for entry in entries:
+                if query.upper() in entry.upper():
+                    matches.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "entries" : matches
+            })
